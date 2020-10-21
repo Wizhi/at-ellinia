8effba41b294655217e788a464eb9d00d9ce196e
Simon Bo Dall Mikkelsen <sm@wesoft.dk>
2020-10-21 19:46:53 +0200

    Treat base16-shell as zsh plugin

--- .local/share/zsh/rc.d/10-base16.zsh
+++ .local/share/zsh/rc.d/10-base16.zsh
@@ -1,5 +1,1 @@
-BASE16_SHELL="$XDG_DATA_HOME/base16-shell/"
-
-[ -n "$PS1" ] && \
-	[ -s "$BASE16_SHELL/profile_helper.sh" ] && \
-		eval "$("$BASE16_SHELL/profile_helper.sh")"
+eval "$("${ZSH_PLUGINS}/wizhi/base16-shell/profile_helper.sh")"
