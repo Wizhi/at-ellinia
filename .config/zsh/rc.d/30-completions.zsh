74e3f06f3e192c69c45f0f760bfefea4d3768f64
Simon Mikkelsen <simonbodall@runbox.com>
2020-11-02 22:28:14 +0100

    Add fuzzy/case insensitive completion
    Sadly fuzzy _and_ case insensitive don't seem to work at the same time,
    and I have no idea why. I'll have to read up on it.

--- .config/zsh/rc.d/30-completions.zsh
+++ .config/zsh/rc.d/30-completions.zsh
@@ -5,6 +5,15 @@ autoload -Uz compinit
 zstyle ':completion:*' menu yes select=10
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
+# Case-insensitive completion
+unsetopt CASE_GLOB
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Fuzzy match mistyped completions.
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
 zmodload zsh/complist
 
 # Only dump completion cache once a day
