370cb15e365af8300f29c3134da97de1381d50c1
Simon Mikkelsen <simonbodall@runbox.com>
2021-04-17 19:20:55 +0200

    Add some anti-fingerprint measures

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -7,9 +7,13 @@ config.load_autoconfig(False)
 config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
 config.set('content.cookies.accept', 'all', 'devtools://*')
 
+config.set('content.headers.user_agent', 'Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0')
+config.set('content.headers.accept_language', 'en-US,en;q=0.5')
+
+config.set('content.canvas_reading', False)
+config.set('content.webgl', False)
 config.set('content.images', True, 'chrome-devtools://*')
 config.set('content.images', True, 'devtools://*')
-
 config.set('content.javascript.enabled', True, 'chrome-devtools://*')
 config.set('content.javascript.enabled', True, 'devtools://*')
 config.set('content.javascript.enabled', True, 'chrome://*/*')
